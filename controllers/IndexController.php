c16701821685b0af430eb91b4ea3595bdfd2bd8a
Al Ev <alexey_e@mail.ru>
2024-10-20 08:44:44 +0300

    making main page

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -8,6 +8,8 @@ include_once 'models/CategoriesModel.php';
 
 function indexAction($smarty)  {
     $allCategories = getAllCategories();
+    $lastProducts = getLastProducts(LASTPRODUCTS);
+    
     $smarty->assign('allCategories', $allCategories);    
     $smarty->assign('pageTitle', 'Главная');
     loadTemplate($smarty, 'index');
